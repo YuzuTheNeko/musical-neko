fc05fddd9e45918bd41bae26ced12c5afa8b2130
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2022-08-05 17:49:40 +0200

    made slashes global

--- src/events/discord.js/ready.ts
+++ src/events/discord.js/ready.ts
@@ -6,7 +6,7 @@ export default createDiscordEventListener("messageCreate", async function() {
     this.manager.loadCommands()
     this.manager.lavalink.start()
 
-    await this.guilds.cache.get(this.config.supportGuildID)!.commands.set(this.manager.asSlashCommands())
+    await this.application.commands.set(this.manager.asSlashCommands())
 
     this.log(`Ready on client ${this.user!.tag} and loaded ${toPluralAmount('command', this.manager.commands.size)}!`)
 })
